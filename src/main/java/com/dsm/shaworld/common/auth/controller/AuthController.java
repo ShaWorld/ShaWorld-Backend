16e1c4d993cf4a5c497f0802f5649020e959c70f
Sonwanseo <sonwanseo@gmail.com>
2021-03-19 15:30:53 +0900

    [Add] <AuthController.java> default Setting

---
+++ src/main/java/com/dsm/shaworld/common/auth/controller/AuthController.java
@@ -0,0 +1,16 @@
+package com.dsm.shaworld.common.auth.controller;
+
+import com.dsm.shaworld.common.auth.service.AuthService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+}
