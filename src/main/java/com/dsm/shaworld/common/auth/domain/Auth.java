7257628bb56fb6c56d7c0de155b87b4b8773779d
Sonwanseo <sonwanseo@gmail.com>
2021-03-19 15:50:15 +0900

    [Add] <AuthDomain> default Setting

---
+++ src/main/java/com/dsm/shaworld/common/auth/domain/Auth.java
@@ -0,0 +1,64 @@
+package com.dsm.shaworld.common.auth.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "auth")
+public class Auth {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "user_email")
+    private String userEmail;
+
+    @Column(name = "user_nickname")
+    private String userNickname;
+
+    @Column(name = "user_password")
+    private String userPassword;
+
+    @Column(name = "user_profile")
+    private String userProfile;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserNickname() {
+        return userNickname;
+    }
+
+    public void setUserNickname(String userNickname) {
+        this.userNickname = userNickname;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserProfile() {
+        return userProfile;
+    }
+
+    public void setUserProfile(String userProfile) {
+        this.userProfile = userProfile;
+    }
+}
