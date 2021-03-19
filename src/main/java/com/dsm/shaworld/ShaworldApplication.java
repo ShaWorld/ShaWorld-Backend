32189e128887f009f8df36c2d1a12f6017768770
Sonwanseo <sonwanseo@gmail.com>
2021-03-19 14:43:38 +0900

    [Init] Default Setting

---
+++ src/main/java/com/dsm/shaworld/ShaworldApplication.java
@@ -0,0 +1,13 @@
+package com.dsm.shaworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShaworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShaworldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dsm/shaworld/ShaworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsm.shaworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShaworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
