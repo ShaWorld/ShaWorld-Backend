9c273dac3b23b912b06ca28d6323646f439d667b
Sonwanseo <sonwanseo@gmail.com>
2021-03-20 14:24:07 +0900

    [Add] <JwtProvider> jwt provider setting

---
+++ src/main/java/com/dsm/shaworld/global/security/JwtProvider.java
@@ -0,0 +1,68 @@
+package com.dsm.shaworld.global.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+    @Value("${auth.jwt.secret}")
+    private String secretKey;
+
+    private final long JWT_ACCESS_EXPIRATION = 10 * 60; // 10분
+    private final long JWT_REFRESH_EXPIRATION = 24 * 60 * 60 * 7; // 1주일
+
+    public String generateAccessToken(String userEmail) {
+        return Jwts.builder()
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_ACCESS_EXPIRATION * 1000))
+                .setSubject(userEmail)
+                .claim("type", "access_token")
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public String generateRefreshToken(String userEmail) {
+        return Jwts.builder()
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_REFRESH_EXPIRATION * 1000))
+                .setSubject(userEmail)
+                .claim("type", "refresh_token")
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if(bearerToken != null && bearerToken.startsWith("Bearer")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+            return true;
+        } catch(Exception e) {
+            return false;
+        }
+    }
+
+    public boolean isRefreshToken(String token) {
+        try {
+            return Jwts.parser().setSigningKey(secretKey)
+                    .parseClaimsJws(token).getBody().get("type").equals("refresh_token");
+        } catch(Exception e) {
+            return false;
+        }
+    }
+
+    public String getId(String token) {
+        return String.valueOf(Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject());
+    }
+}
