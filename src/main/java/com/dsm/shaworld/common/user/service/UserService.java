b2a55098da2e422f7ddb394d090a062f75860d59
Sonwanseo <sonwanseo@gmail.com>
2021-03-21 23:30:04 +0900

    <UserService> add service signUp Func

--- src/main/java/com/dsm/shaworld/common/user/service/UserService.java
+++ src/main/java/com/dsm/shaworld/common/user/service/UserService.java
@@ -1,6 +1,8 @@
 package com.dsm.shaworld.common.user.service;
 
+import com.dsm.shaworld.common.user.dto.SignUpRequest;
 import com.dsm.shaworld.common.user.repository.UserRepository;
+import com.dsm.shaworld.global.exception.PasswordMismatchException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +13,8 @@ public class UserService {
         this.authRepository = authRepository;
     }
 
-    public void signUp() {
-
+    public void signUp(SignUpRequest request) {
+        if(request.getPassword() != request.getPasswordConfirm())
+            throw new PasswordMismatchException(request.getPassword(), request.getPasswordConfirm());
     }
 }
