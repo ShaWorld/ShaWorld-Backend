4dfbc85a7e338f02589899cb8f07bc3e67b619e5
Sonwanseo <sonwanseo@gmail.com>
2021-03-21 23:25:38 +0900

    <UserController> add controller signUp Func

--- src/main/java/com/dsm/shaworld/common/user/controller/UserController.java
+++ src/main/java/com/dsm/shaworld/common/user/controller/UserController.java
@@ -1,20 +1,21 @@
 package com.dsm.shaworld.common.user.controller;
 
+import com.dsm.shaworld.common.user.dto.SignUpRequest;
 import com.dsm.shaworld.common.user.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/user")
 public class UserController {
 
-    private final UserService authService;
+    private final UserService userService;
 
-    public UserController(UserService authService) {
-        this.authService = authService;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @PostMapping("signUp")
-    public void signUp() {
-
+    public void signUp(@RequestBody SignUpRequest request) {
+        userService.signUp(request);
     }
 }
