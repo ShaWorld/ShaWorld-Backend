fec6861b6a5ca20134c33f0aa5993d8c4a912c71
Sonwanseo <sonwanseo@gmail.com>
2021-03-22 17:15:29 +0900

    <EmailDuplicateException> add emailDuplicateException

---
+++ src/main/java/com/dsm/shaworld/global/exception/EmailDuplicateException.java
@@ -0,0 +1,14 @@
+package com.dsm.shaworld.global.exception;
+
+import com.dsm.shaworld.global.exception.handler.CommonException;
+import org.springframework.http.HttpStatus;
+
+public class EmailDuplicateException extends CommonException {
+
+    public EmailDuplicateException(String email) {
+        super(
+            "EMAIL_DUPLICATION",
+            String.format("이미 사용중인 이메일입니다. [%s]", email),
+            HttpStatus.CONFLICT);
+    }
+}
