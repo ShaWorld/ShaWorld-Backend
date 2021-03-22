448fadc5e980c20ec1250c76a390d46ae409c0b9
Sonwanseo <sonwanseo@gmail.com>
2021-03-22 17:12:58 +0900

    <UserRepository> add JPA func

--- src/main/java/com/dsm/shaworld/common/user/repository/UserRepository.java
+++ src/main/java/com/dsm/shaworld/common/user/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.dsm.shaworld.common.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    boolean existsByUserEmail(String userEmail);
 }
