fea16efa6ded69a699fa5ac1d5dfbdad5c9fc06d
Sonwanseo <sonwanseo@gmail.com>
2021-03-22 17:14:43 +0900

    <PasswordMismatchException> make code look better

--- src/main/java/com/dsm/shaworld/global/exception/PasswordMismatchException.java
+++ src/main/java/com/dsm/shaworld/global/exception/PasswordMismatchException.java
@@ -6,6 +6,11 @@ import org.springframework.http.HttpStatus;
 public class PasswordMismatchException extends CommonException {
 
     public PasswordMismatchException(String password, String passwordConfirm) {
-        super("PASSWORD_MISMATCH", String.format("비밀번호가 일치하지 않습니다. [%s, %s]", password, passwordConfirm), HttpStatus.BAD_REQUEST);
+        super(
+            "PASSWORD_MISMATCH",
+            String.format("비밀번호가 일치하지 않습니다. [%s, %s]", password, passwordConfirm),
+            HttpStatus.BAD_REQUEST
+        );
     }
+
 }
