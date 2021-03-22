51390f309346ca8a74a01f457467a3d85daa94b9
Sonwanseo <sonwanseo@gmail.com>
2021-03-22 17:14:08 +0900

    <CommonExceptionHandler> remove error

--- src/main/java/com/dsm/shaworld/global/exception/handler/CommonExceptionHandler.java
+++ src/main/java/com/dsm/shaworld/global/exception/handler/CommonExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.dsm.shaworld.global.exception.handler;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 
@@ -7,10 +8,17 @@ import org.springframework.http.ResponseEntity;
 public class CommonExceptionHandler {
 
     @ExceptionHandler(CommonException.class)
-    public ResponseEntity commonExceptionHandler(CommonException e) {
+    public ResponseEntity<ExceptionResponse> commonExceptionHandler(CommonException e) {
         return new ResponseEntity(
             new ExceptionResponse(e.getCode(), e.getMessage()),
             e.getStatus()
         );
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ExceptionResponse runtimeExceptionHandler(RuntimeException e) {
+        e.printStackTrace();
+        return new ExceptionResponse("INTERNAL_SERVER_ERROR", "문제가 발생하였다.");
     }
 }
