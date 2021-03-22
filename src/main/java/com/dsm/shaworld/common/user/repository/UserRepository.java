25981349b09f098570fe2a52e3bdd315d0d7b6bb
Sonwanseo <sonwanseo@gmail.com>
2021-03-22 23:20:12 +0900

    <UserRepository> add findByUserEmail func

--- src/main/java/com/dsm/shaworld/common/user/repository/UserRepository.java
+++ src/main/java/com/dsm/shaworld/common/user/repository/UserRepository.java
@@ -3,7 +3,10 @@ package com.dsm.shaworld.common.user.repository;
 import com.dsm.shaworld.common.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUserEmail(String userEmail);
     boolean existsByUserEmail(String userEmail);
     boolean existsByUserNickname(String userNickname);
 }
