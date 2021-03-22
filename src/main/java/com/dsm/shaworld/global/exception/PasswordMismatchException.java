484152baecb742a20fd8203c6a1727ca65e0a2d5
Sonwanseo <sonwanseo@gmail.com>
2021-03-22 23:22:58 +0900

    <PasswordMismatchException> add overloading constructor

--- src/main/java/com/dsm/shaworld/global/exception/PasswordMismatchException.java
+++ src/main/java/com/dsm/shaworld/global/exception/PasswordMismatchException.java
@@ -5,6 +5,14 @@ import org.springframework.http.HttpStatus;
 
 public class PasswordMismatchException extends CommonException {
 
+    public PasswordMismatchException(String password) {
+        super(
+                "PASSWORD_MISMATCH",
+                String.format("비밀번호가 일치하지 않습니다. [%s]", password),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
     public PasswordMismatchException(String password, String passwordConfirm) {
         super(
             "PASSWORD_MISMATCH",
