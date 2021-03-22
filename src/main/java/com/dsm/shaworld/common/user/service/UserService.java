f503c460b632de28e2b49ef38670e6ed946b463f
Sonwanseo <sonwanseo@gmail.com>
2021-03-22 23:13:28 +0900

    <UserService> remove interface

--- src/main/java/com/dsm/shaworld/common/user/service/UserService.java
+++ src/main/java/com/dsm/shaworld/common/user/service/UserService.java
@@ -1,9 +1,60 @@
 package com.dsm.shaworld.common.user.service;
 
+import com.dsm.shaworld.common.user.dto.SignInRequest;
+import com.dsm.shaworld.common.user.dto.SignInResponse;
 import com.dsm.shaworld.common.user.dto.SignUpRequest;
+import com.dsm.shaworld.common.user.entity.User;
+import com.dsm.shaworld.common.user.repository.UserRepository;
+import com.dsm.shaworld.global.authorization.JwtProvider;
+import com.dsm.shaworld.global.exception.EmailDuplicateException;
+import com.dsm.shaworld.global.exception.NicknameDuplicateException;
+import com.dsm.shaworld.global.exception.PasswordMismatchException;
+import com.dsm.shaworld.global.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
-public interface UserService {
-    boolean emailDuplicationCheck(String userEmail);
-    boolean nicknameDuplicationCheck(String userNickname);
-    void signUp(SignUpRequest request);
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final JwtProvider jwtProvider;
+
+    public User getInfoByUserEmail(String userEmail) {
+        return userRepository.findByUserEmail(userEmail).orElseThrow(() -> new UserNotFoundException(userEmail));
+    }
+
+    public SignInResponse signIn(SignInRequest request) {
+        User user = getInfoByUserEmail(request.getEmail());
+        if(!request.getPassword().equals(user.getUserPassword()))
+            throw new PasswordMismatchException(request.getPassword());
+        return SignInResponse.builder()
+            .accessToken(jwtProvider.generateAccessToken(request.getEmail()))
+            .refreshToken(jwtProvider.generateRefreshToken(request.getEmail()))
+            .build();
+    }
+
+    public boolean emailDuplicationCheck(String userEmail) {
+        return userRepository.existsByUserEmail(userEmail);
+    }
+
+    public boolean nicknameDuplicationCheck(String userNickname) {
+        return userRepository.existsByUserNickname(userNickname);
+    }
+
+    public void signUp(SignUpRequest request) {
+        if(!request.getPassword().equals(request.getPasswordConfirm()))
+            throw new PasswordMismatchException(request.getPassword(), request.getPasswordConfirm());
+        if(emailDuplicationCheck(request.getEmail()))
+            throw new EmailDuplicateException(request.getEmail());
+        if(nicknameDuplicationCheck(request.getNickname()))
+            throw new NicknameDuplicateException(request.getNickname());
+
+        userRepository.save(
+            User.builder()
+            .userEmail(request.getEmail())
+            .userNickname(request.getNickname())
+            .userPassword(request.getPassword())
+            .build()
+        );
+    }
 }
