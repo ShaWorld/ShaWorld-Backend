a8ea5700a54c5b6cde2b8a013bca048cdf1f7ab8
Sonwanseo <sonwanseo@gmail.com>
2021-03-22 09:35:23 +0900

    <UserController> change file name

--- src/main/java/com/dsm/shaworld/common/user/controller/UserController.java
+++ src/main/java/com/dsm/shaworld/common/user/controller/UserController.java
@@ -1,7 +1,7 @@
 package com.dsm.shaworld.common.user.controller;
 
 import com.dsm.shaworld.common.user.dto.SignUpRequest;
-import com.dsm.shaworld.common.user.service.UserService;
+import com.dsm.shaworld.common.user.service.UserServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,9 +11,9 @@ import javax.validation.Valid;
 @RequestMapping("/user")
 public class UserController {
 
-    private final UserService userService;
+    private final UserServiceImpl userService;
 
-    public UserController(UserService userService) {
+    public UserController(UserServiceImpl userService) {
         this.userService = userService;
     }
 
