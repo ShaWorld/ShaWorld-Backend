d7db5b826e37f3275637345e6d78510e6c4fc7e7
Sonwanseo <sonwanseo@gmail.com>
2021-03-22 17:13:23 +0900

    <UserService> add duplicationCheck func interface

--- src/main/java/com/dsm/shaworld/common/user/service/UserService.java
+++ src/main/java/com/dsm/shaworld/common/user/service/UserService.java
@@ -3,5 +3,6 @@ package com.dsm.shaworld.common.user.service;
 import com.dsm.shaworld.common.user.dto.SignUpRequest;
 
 public interface UserService {
+    boolean emailDuplicationCheck(String userEmail);
     void signUp(SignUpRequest request);
 }
