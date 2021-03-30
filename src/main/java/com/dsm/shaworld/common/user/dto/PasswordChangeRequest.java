4357abac7bc4e8916e7b7bc815e89026f4e0b6b6
Sonwanseo <sonwanseo@gmail.com>
2021-03-30 15:41:32 +0900

    <PasswordChangeRequest> create passwordChangeRequest DTO

---
+++ src/main/java/com/dsm/shaworld/common/user/dto/PasswordChangeRequest.java
@@ -0,0 +1,21 @@
+package com.dsm.shaworld.common.user.dto;
+
+import lombok.Getter;
+
+import javax.validation.constraints.*;
+
+@Getter
+public class PasswordChangeRequest {
+    @NotBlank
+    @Pattern(regexp = "^[a-z0-9!@#$%^&*]{8,20}$")
+    private String currentPassword;
+
+    @NotBlank
+    @Pattern(regexp = "^[a-z0-9!@#$%^&*]{8,20}$")
+    private String changedPassword;
+
+    @NotBlank
+    @Pattern(regexp = "^[a-z0-9!@#$%^&*]{8,20}$")
+    private String changedPasswordConfirm;
+
+}
