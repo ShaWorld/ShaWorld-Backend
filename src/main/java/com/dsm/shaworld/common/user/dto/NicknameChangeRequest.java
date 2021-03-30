a88bd45095ce6fbb0ae2619afe332965631da10d
Sonwanseo <sonwanseo@gmail.com>
2021-03-30 15:41:16 +0900

    <NicknameChangeRequest> create nicknameChangeRequest DTO

---
+++ src/main/java/com/dsm/shaworld/common/user/dto/NicknameChangeRequest.java
@@ -0,0 +1,13 @@
+package com.dsm.shaworld.common.user.dto;
+
+import lombok.Getter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+public class NicknameChangeRequest {
+    @NotBlank
+    @Size(min = 2, max = 8)
+    private String changedNickname;
+}
