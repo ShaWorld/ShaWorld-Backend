3c7118c21a0aed71fefa83a9bedcadf1342eb768
Sonwanseo <sonwanseo@gmail.com>
2021-03-31 23:50:38 +0900

    <ProfileChangeRequest> create image s3Uploader

--- src/main/java/com/dsm/shaworld/domain/user/dto/ProfileChangeRequest.java
+++ src/main/java/com/dsm/shaworld/domain/user/dto/ProfileChangeRequest.java
@@ -1,11 +1,16 @@
 package com.dsm.shaworld.domain.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Getter
+@Setter
 public class ProfileChangeRequest {
-    @NotBlank
-    private String changedProfile;
+    @NotNull
+    private MultipartFile profile;
 }
