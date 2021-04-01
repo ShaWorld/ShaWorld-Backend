677d16227bf004cf46705229e8a17e892b736051
Sonwanseo <sonwanseo@gmail.com>
2021-04-01 23:00:04 +0900

    <PostController> createPost

--- src/main/java/com/dsm/shaworld/domain/post/controller/PostController.java
+++ src/main/java/com/dsm/shaworld/domain/post/controller/PostController.java
@@ -3,9 +3,12 @@ package com.dsm.shaworld.domain.post.controller;
 import com.dsm.shaworld.domain.post.dto.CreatePostRequest;
 import com.dsm.shaworld.domain.post.service.PostService;
 import org.springframework.http.HttpStatus;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import java.io.IOException;
 
 @CrossOrigin
 @RestController
@@ -19,7 +22,8 @@ public class PostController {
 
     @PostMapping("/create")
     @ResponseStatus(value = HttpStatus.CREATED)
-    public void createPost(@RequestHeader(value="Authorization") String token, @RequestBody @Valid CreatePostRequest request) {
-        postService.createPost(token, request);
+    public void createPost(@RequestHeader(value="Authorization") String token, @RequestBody @Valid CreatePostRequest request, @RequestPart(name="thumbnail") @Valid MultipartFile file) throws IOException {
+        System.out.println(file);
+        postService.createPost(token, request, file);
     }
 }
