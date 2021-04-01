737f19c99261c80d345a882a4423278899634ac2
Sonwanseo <sonwanseo@gmail.com>
2021-04-01 23:50:28 +0900

    <PostController> get data from requestParam

--- src/main/java/com/dsm/shaworld/domain/post/controller/PostController.java
+++ src/main/java/com/dsm/shaworld/domain/post/controller/PostController.java
@@ -1,14 +1,13 @@
 package com.dsm.shaworld.domain.post.controller;
 
-import com.dsm.shaworld.domain.post.dto.CreatePostRequest;
 import com.dsm.shaworld.domain.post.service.PostService;
 import org.springframework.http.HttpStatus;
-import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 @CrossOrigin
 @RestController
@@ -22,8 +21,15 @@ public class PostController {
 
     @PostMapping("/create")
     @ResponseStatus(value = HttpStatus.CREATED)
-    public void createPost(@RequestHeader(value="Authorization") String token, @RequestBody @Valid CreatePostRequest request, @RequestPart(name="thumbnail") @Valid MultipartFile file) throws IOException {
-        System.out.println(file);
-        postService.createPost(token, request, file);
+    public void createPost(
+        @RequestHeader(value="Authorization") String token,
+        @RequestParam(value="title") String title,
+        @RequestParam(value="address") String address,
+        @RequestParam(value="detail") String detail,
+        @RequestParam(value="price") int price,
+        @RequestParam(value="date") LocalDateTime date,
+        @RequestPart(name="thumbnail") @Valid MultipartFile file
+    ) throws IOException {
+        postService.createPost(token, title, address, detail, price, date, file);
     }
 }
