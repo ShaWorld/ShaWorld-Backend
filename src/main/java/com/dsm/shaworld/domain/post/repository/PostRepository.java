0628d2755c5ce614b486b92a2d721ce6c2eb1c0c
Sonwanseo <sonwanseo@gmail.com>
2021-04-04 23:48:36 +0900

    <PostRepository> findByPostId

--- src/main/java/com/dsm/shaworld/domain/post/repository/PostRepository.java
+++ src/main/java/com/dsm/shaworld/domain/post/repository/PostRepository.java
@@ -3,5 +3,8 @@ package com.dsm.shaworld.domain.post.repository;
 import com.dsm.shaworld.domain.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PostRepository extends JpaRepository<Post, Integer> {
+    Optional<Post> findByPostId(int postId);
 }
