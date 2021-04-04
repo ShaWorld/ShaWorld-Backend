00d3c50a003b2e7388f47a459c580c5fcb4ec2c8
Sonwanseo <sonwanseo@gmail.com>
2021-04-04 23:45:18 +0900

    <PostService> create getPost func

--- src/main/java/com/dsm/shaworld/domain/post/service/PostService.java
+++ src/main/java/com/dsm/shaworld/domain/post/service/PostService.java
@@ -1,9 +1,12 @@
 package com.dsm.shaworld.domain.post.service;
 
+import com.dsm.shaworld.domain.post.dto.GetPostResponse;
 import com.dsm.shaworld.domain.post.entity.Post;
 import com.dsm.shaworld.domain.post.repository.PostRepository;
+import com.dsm.shaworld.domain.user.dto.UserInfoResponse;
 import com.dsm.shaworld.domain.user.entity.User;
 import com.dsm.shaworld.domain.user.service.UserService;
+import com.dsm.shaworld.global.exception.handler.PostNotFoundException;
 import com.dsm.shaworld.global.s3service.S3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -39,4 +42,24 @@ public class PostService {
         return;
     }
 
+    public GetPostResponse getPost(int postId) {
+        Post post = postRepository.findByPostId(postId).orElseThrow(() -> new PostNotFoundException(postId));
+
+        UserInfoResponse author =
+            UserInfoResponse.builder()
+            .userEmail(post.getPostAuthor().getUserEmail())
+            .userNickname(post.getPostAuthor().getUserNickname())
+            .userProfile(post.getPostAuthor().getUserProfile())
+            .build();
+
+        return GetPostResponse.builder()
+            .postThumbnail(post.getPostThumbnail())
+            .postTitle(post.getPostTitle())
+            .postAuthor(author)
+            .postAddress(post.getPostAddress())
+            .postDetail(post.getPostDetail())
+            .postPrice(post.getPostPrice())
+            .postDate(post.getPostDate())
+            .build();
+    }
 }
