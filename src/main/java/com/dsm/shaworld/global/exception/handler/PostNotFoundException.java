258bb590852aa10686cd3b817bfd9a0db34af3f3
Sonwanseo <sonwanseo@gmail.com>
2021-04-04 23:40:47 +0900

    <PostNotFoundException> create PostNotFoundException

---
+++ src/main/java/com/dsm/shaworld/global/exception/handler/PostNotFoundException.java
@@ -0,0 +1,14 @@
+package com.dsm.shaworld.global.exception.handler;
+
+import org.springframework.http.HttpStatus;
+
+public class PostNotFoundException extends CommonException {
+
+    public PostNotFoundException(int postId) {
+        super(
+            "POST_NOT_FOUND",
+            String.format("게시물을 찾을 수 없습니다. [%d]", postId),
+            HttpStatus.NOT_FOUND
+        );
+    }
+}
