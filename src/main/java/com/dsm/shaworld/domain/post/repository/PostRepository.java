6ef274138f9f88abe04b0551a20be3ec9142b0d4
Sonwanseo <sonwanseo@gmail.com>
2021-04-05 22:30:56 +0900

    <PostRepository> create pagenation func

--- src/main/java/com/dsm/shaworld/domain/post/repository/PostRepository.java
+++ src/main/java/com/dsm/shaworld/domain/post/repository/PostRepository.java
@@ -1,10 +1,14 @@
 package com.dsm.shaworld.domain.post.repository;
 
 import com.dsm.shaworld.domain.post.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
     Optional<Post> findByPostId(int postId);
+
+    Page<Post> findAll(Pageable pageable);
 }
