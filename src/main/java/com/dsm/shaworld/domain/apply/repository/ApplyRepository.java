a9fb19aa40b41c98de40d4b1a7c0a9e50a18c758
Sonwanseo <sonwanseo@gmail.com>
2021-04-14 23:37:53 +0900

    <ApplyRepository> create ApplyRepository

---
+++ src/main/java/com/dsm/shaworld/domain/apply/repository/ApplyRepository.java
@@ -0,0 +1,14 @@
+package com.dsm.shaworld.domain.apply.repository;
+
+import com.dsm.shaworld.domain.apply.entity.Apply;
+import com.dsm.shaworld.domain.post.entity.Post;
+import com.dsm.shaworld.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ApplyRepository extends JpaRepository<Apply, Integer> {
+    List<Apply> findByApplyApplicant(User user);
+    List<Apply> findByApplyPostPostAuthor(User user);
+    boolean existsByApplyPostAndApplyApplicant(Post post, User user);
+}
