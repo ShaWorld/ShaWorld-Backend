b3e65ca3a3304f910bf740dff240edc5270bd152
Sonwanseo <sonwanseo@gmail.com>
2021-04-14 23:37:04 +0900

    <GetSendAppliesResponse> create GetSendAppliesResponse DTO

---
+++ src/main/java/com/dsm/shaworld/domain/apply/dto/GetSendAppliesResponse.java
@@ -0,0 +1,22 @@
+package com.dsm.shaworld.domain.apply.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Builder
+public class GetSendAppliesResponse {
+    @NotBlank
+    private int applyId;
+
+    @NotBlank
+    private int applyPostId;
+
+    @NotBlank
+    private String applyPostTitle;
+
+    @NotBlank
+    private String applyState;
+}
