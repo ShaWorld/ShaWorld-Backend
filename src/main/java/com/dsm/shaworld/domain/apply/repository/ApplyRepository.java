93c16d7d8d3eebb8b314c068a04948332ad662d4
Sonwanseo <sonwanseo@gmail.com>
2021-04-15 10:47:57 +0900

    <ApplyRepository> add a func that returns Apply Entity

--- src/main/java/com/dsm/shaworld/domain/apply/repository/ApplyRepository.java
+++ src/main/java/com/dsm/shaworld/domain/apply/repository/ApplyRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ApplyRepository extends JpaRepository<Apply, Integer> {
+    Apply findByApplyId(int applyId);
     List<Apply> findByApplyApplicant(User user);
     List<Apply> findByApplyPostPostAuthor(User user);
     boolean existsByApplyPostAndApplyApplicant(Post post, User user);
